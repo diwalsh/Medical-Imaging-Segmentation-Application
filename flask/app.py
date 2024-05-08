2de6762b136cc996acf5bbe3f37b04afe823f9d6
diwalsh <112559744+diwalsh@users.noreply.github.com>
2024-05-08 13:56:38 +0200

    adding python and html templates

---
+++ flask/app.py
@@ -0,0 +1,361 @@
+import os
+import zipfile
+import base64
+import cv2
+import tempfile
+import torch
+import re
+import time
+import json
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from cs50 import SQL
+from helpers import apology, login_required, normalize, resize, get_patient_images, generate_bouncing_gif
+from model_v2 import predict
+from threed import load_images_from_folder, threed_render
+import logging
+
+# # Set the logging level to WARNING to suppress debug and info messages
+# logging.basicConfig(level=logging.WARNING)
+# # Set the logging level for the Hugging Face Transformers library to WARNING
+# transformers_logger = logging.getLogger("transformers")
+# transformers_logger.setLevel(logging.WARNING)
+# # Set the logging level for the Werkzeug server to WARNING
+# werkzeug_logger = logging.getLogger("werkzeug")
+# werkzeug_logger.setLevel(logging.WARNING)
+# # Suppress debug messages from Matplotlib and TensorFlow
+# logging.getLogger('matplotlib').setLevel(logging.WARNING)
+# logging.getLogger('tensorflow').setLevel(logging.WARNING)
+
+
+# configure app
+app = Flask(__name__, static_url_path='/static')
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# use CS50 built in Library to connect to database via sqlite
+# (handles messy sqlachemy connection openings/closures for you)
+db = SQL("sqlite:///dats.db")
+
+# Directory to save the generated renderings, etc. 
+# this will be fleshed out later 
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# ensure the upload folder exists!! lol
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+organ_colors = [[249, 187, 191], 
+                [254, 128, 162],
+                [183, 82, 100]] 
+    
+# goodbye cache, hello css 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/upload", methods=["GET", "POST"])
+@login_required
+def upload():
+    # User reached route via POST (as by submitting a form)
+    if request.method == "POST":
+        file = request.files['data_zip_file']
+        file_like_object = file.stream._file  
+        zipfile_ob = zipfile.ZipFile(file_like_object)
+        file_names = zipfile_ob.namelist()
+        # Filter names to only include the filetype that you want:
+        file_names = [file_name for file_name in file_names if file_name.endswith(".png")]
+        files = [] 
+        for name in file_names:
+            if not name.startswith("__MACOSX"):
+                img_data = zipfile_ob.open(name).read()
+                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                    temp_file.write(img_data)
+                    temp_file_path = temp_file.name
+                # the below img's need to be sent to a resizing function
+                # to be fed into a model for segmentation     
+                img = normalize(temp_file_path)
+                # from here on, the normalized imgs are prepared for showing on screen to user
+                img_data_base64 = cv2.imencode('.png', img)[1].tostring()
+                img_data_base64 = base64.b64encode(img_data_base64).decode('utf-8')
+                # grabbing relevant data from file name to format for user display
+                parts = name.split("/")[-1].split("_")
+                case_number = ''.join(filter(str.isdigit, parts[0]))
+                day_number = ''.join(filter(str.isdigit, parts[1]))
+                slice_number = int(parts[3])
+                formatted_name = "Case {}; Day {}: Slice {}".format(case_number, day_number, slice_number)
+                # Create a unique filename for the image
+                user_id = str(session["user_id"])
+                # pop off any prefixed folder names
+                file_name = name.split("/")[-1]
+                # Remove the .png suffix from the filename
+                file_name = file_name.split(".png")[0]
+                img_filename = "{}_{}.png".format(file_name, user_id)
+                # create the normalized folder if not already there
+                normalized_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'normalized')
+                if not os.path.exists(normalized_folder):
+                    os.makedirs(normalized_folder)
+                img_path = os.path.join(normalized_folder, img_filename)
+                # Save the image to the normalized folder
+                cv2.imwrite(img_path, img)
+                # Append printable image, formatted name, and original name for display use
+                files.append((img_data_base64, formatted_name, img_path))
+                os.unlink(temp_file_path)  # Delete the temporary file after use
+        # Sort files based on the original file name
+        files = sorted(files, key=lambda x: x[2])
+        session["uploaded_files"] = files
+        # Retrieve image paths
+        folder_path = "static/uploads/normalized"
+        image_paths = get_patient_images(case_number, day_number, folder_path, user_id)
+        # Store image paths in session
+        session['image_paths'] = image_paths
+        return render_template("pngs.html", files=files)
+    else:
+        return render_template("upload.html")
+
+@app.route("/model")
+@login_required
+def model():
+    start = time.perf_counter()
+    # retrieve image paths from session, declare device, checkpoint and model
+    image_paths = session.get('image_paths', [])
+    # predict!!
+    overlay_image_paths = predict(image_paths)
+    session['overlay_paths'] = overlay_image_paths
+    
+    # Convert images to base64
+    predictions = []
+    for img_path in overlay_image_paths:
+        # Extract formatted name from filename
+        filename = os.path.splitext(os.path.basename(img_path))[0]
+        parts = filename.split("_")
+        case_number = ''.join(filter(str.isdigit, parts[0]))
+        day_number = ''.join(filter(str.isdigit, parts[1]))
+        slice_number = int(parts[3].split(".")[0])
+        formatted_name = "Case {}; Day {}: Slice {}".format(case_number, day_number, slice_number)
+        
+        img_name = os.path.basename(img_path)  # Extract file name (no path)
+        
+        predictions.append((img_path, formatted_name, img_name))
+    
+    # Sort the list of file names
+    predictions = sorted(predictions, key=lambda x: x[2])
+    
+    # set folder for output and filename for export
+    output_folder = 'static/uploads/objs'
+    static_folder = 'uploads/objs'
+    combined_filename = os.path.join(output_folder, f'case{case_number}_day{day_number}.obj') # make dynamic
+    static_filename = os.path.join(static_folder, f'case{case_number}_day{day_number}.obj')
+    session['obj_path'] = os.path.splitext(static_filename)[0]
+
+    # Set the folder where your images are stored
+    image_folder = 'static/uploads/masks'  # replace with the path to your image folder
+    prefix = f'case{case_number}_day{day_number}' # make dynamic
+
+    images = load_images_from_folder(image_folder, prefix)
+    threed_render(images, combined_filename, organ_colors)
+    
+    end = time.perf_counter()
+    time_sum = (end - start)
+    minutes, seconds = divmod(time_sum, 60)
+    print(f"Time elapsed: {int(minutes)} minutes and {seconds:.2f} seconds")
+
+    return render_template("model.html", predictions=predictions, image_paths=image_paths)
+
+@app.route("/render")
+@login_required
+def render():
+    # retrieve image paths from session, declare device, checkpoint and model
+    image_paths = sorted(session.get('overlay_paths', []))
+    obj_path = session.get('obj_path')
+    user_id = session.get('user_id')
+    # Calculate the middle index
+    middle_index = len(image_paths) // 2
+    # Access the middle image path for later use of cover image
+    cover_image_path = image_paths[middle_index]
+    
+    parts = image_paths[0].split("/")[-1].split("_")
+    case_number = ''.join(filter(str.isdigit, parts[0]))
+    day_number = ''.join(filter(str.isdigit, parts[1]))
+    title = "Case {}; Day {}".format(case_number, day_number)
+    
+    # Convert image_paths list to a JSON string
+    mask_paths = json.dumps(image_paths)
+    
+    db.execute("INSERT INTO renderings (user_id, case_name, case_number, day_number, cover_image, mask_paths, obj_path) VALUES (?, ?, ?, ?, ?, ?, ?)",
+               user_id, title, case_number, day_number, cover_image_path, mask_paths, obj_path)
+    
+    # Define generate_title function
+    def generate_title_slice(image_path):
+        parts = image_path.split("/")[-1].split("_")
+        case_number = ''.join(filter(str.isdigit, parts[0]))
+        day_number = ''.join(filter(str.isdigit, parts[1]))
+        slice_number = int(parts[3].split(".")[0])
+        formatted_name = "Case {}; Day {}: Slice {}".format(case_number, day_number, slice_number)
+        return formatted_name
+    
+    return render_template("render2.html", image_paths=image_paths, obj_path=obj_path, title=title, generate_title_slice=generate_title_slice)
+
+@app.route("/archive")
+@login_required
+def archive():
+    user_id = session["user_id"]
+    # renderings = db.execute("SELECT * FROM renderings WHERE user_id = ? ORDER BY created_at DESC", user_id)
+    renderings = db.execute("""
+        SELECT r.*
+        FROM renderings r
+        JOIN (
+            SELECT case_name, MAX(created_at) AS max_created_at
+            FROM renderings
+            WHERE user_id = ?
+            GROUP BY case_name
+        ) m ON r.case_name = m.case_name AND r.created_at = m.max_created_at
+        WHERE r.user_id = ?
+        ORDER BY r.case_number ASC, r.day_number ASC
+    """, user_id, user_id)
+
+    return render_template("archive.html", renderings=renderings)
+
+@app.route("/<int:rendering_id>")
+@login_required
+def view_render(rendering_id):
+    model = db.execute("SELECT * FROM renderings WHERE id = ?", rendering_id)
+    image_paths = json.loads(model[0]['mask_paths'])
+
+    if not model:
+        return apology("Model not found", 404)
+    
+    # Define generate_title function
+    def generate_title_slice(image_path):
+        parts = image_path.split("/")[-1].split("_")
+        case_number = ''.join(filter(str.isdigit, parts[0]))
+        day_number = ''.join(filter(str.isdigit, parts[1]))
+        slice_number = int(parts[3].split(".")[0])
+        formatted_name = "Case {}; Day {}: Slice {}".format(case_number, day_number, slice_number)
+        return formatted_name
+
+    return render_template("render2.html", image_paths=image_paths, obj_path=model[0]['obj_path'], title=model[0]['case_name'], generate_title_slice=generate_title_slice)
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must create username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must create password", 400)
+
+        # Ensure password confirmation was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must repeat password", 400)
+
+        # Ensure passwords match!
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords must match!", 400)
+
+        # Query database for username
+        exists = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username doesn't exist
+        if len(exists) > 0:
+            return apology("username already taken :( ", 400)
+
+        # add user to database
+        username = request.form.get("username")
+        password = generate_password_hash(request.form.get("password"))
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, password)
+
+        # Remember which user has logged in
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+    
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ flask/model.py
@@ -0,0 +1,271 @@
+import os
+import warnings
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+from dataclasses import dataclass
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import lightning.pytorch as pl
+from torchvision import transforms
+from transformers import SegformerForSemanticSegmentation
+
+import tensorflow as tf
+# from tensorflow import keras
+from keras.models import load_model
+# For data augmentation and preprocessing
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+# To filter UserWarning.
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# import logging
+
+# # Set the logging level to WARNING to suppress debug and info messages
+# logging.basicConfig(level=logging.WARNING)
+# # Set the logging level for the Hugging Face Transformers library to WARNING
+# transformers_logger = logging.getLogger("transformers")
+# transformers_logger.setLevel(logging.WARNING)
+# # Set the logging level for the Werkzeug server to WARNING
+# werkzeug_logger = logging.getLogger("werkzeug")
+# werkzeug_logger.setLevel(logging.WARNING)
+# # Suppress debug messages from Matplotlib and TensorFlow
+# logging.getLogger('matplotlib').setLevel(logging.WARNING)
+# logging.getLogger('tensorflow').setLevel(logging.WARNING)
+
+
+
+
+@dataclass(frozen=True)
+class DatasetConfig:
+    NUM_CLASSES:   int = 4 # including background.
+    IMAGE_SIZE: tuple[int,int] = (288, 288) # W, H
+    MEAN: tuple = (0.485, 0.456, 0.406)
+    STD:  tuple = (0.229, 0.224, 0.225)
+    MEAN_CLF: float = 0.136
+    STD_CLF: float = 0.178
+    BACKGROUND_CLS_ID: int = 0
+    THR: float = 0.07652711868286133
+   
+@dataclass
+class InferenceConfig:
+    BATCH_SIZE:  int = 12 # can increase the batch size to find faster optimization 
+    NUM_BATCHES: int = 2
+    
+
+    
+# Create a mapping of class ID to RGB value.
+id2color = {
+    0: (0, 0, 0),    # background pixel
+    1: (255, 140, 140),  # Stomach
+    2: (255, 102, 102),  # Small Bowel
+    3: (255, 51, 51),  # large Bowel
+}
+
+class MedicalSegmentationModel(pl.LightningModule):
+    def __init__(
+        self,
+        model_name: str,
+        num_classes: int = 10,
+        init_lr: float = 0.001,
+        optimizer_name: str = "Adam",
+        weight_decay: float = 1e-4,
+        use_scheduler: bool = False,
+        scheduler_name: str = "multistep_lr",
+        num_epochs: int = 1,
+        verbose: int = 0
+    ):
+        super().__init__()
+
+        # Save the arguments as hyperparameters.
+        self.save_hyperparameters()
+
+        # Loading model using the function defined above.
+        self.model = get_model(model_name=self.hparams.model_name, num_classes=self.hparams.num_classes)
+
+    def forward(self, data):
+        outputs = self.model(pixel_values=data, return_dict=True)
+        upsampled_logits = F.interpolate(outputs["logits"], size=data.shape[-2:], mode="bilinear", align_corners=False)
+        return upsampled_logits
+    
+def get_model(*, model_name, num_classes):
+    model = SegformerForSemanticSegmentation.from_pretrained(
+        model_name,
+        num_labels=num_classes,
+        ignore_mismatched_sizes=True,
+        verbose=0
+    )
+    return model
+
+def num_to_rgb(num_arr, color_map=id2color):
+    single_layer = np.squeeze(num_arr)
+    output = np.zeros(num_arr.shape[:2] + (3,))
+
+    for k in color_map.keys():
+        output[single_layer == k] = color_map[k]
+
+    # return a floating point array in range [0.0, 1.0]
+    return np.float32(output) / 255.0
+
+# Function to overlay a segmentation map on top of an RGB image.
+def image_overlay(image, segmented_image):
+    alpha = 1.0  # Transparency for the original image.
+    beta = 0.7  # Transparency for the segmentation map.
+    gamma = 0.0  # Scalar added to each sum.
+
+    segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    image = cv2.addWeighted(image, alpha, segmented_image, beta, gamma, image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return np.clip(image, 0.0, 1.0)
+
+def normalize_classif(image, mean, std):
+        image = image / 255.0
+        image = (image - mean) / std
+        return image
+
+# load and resize the image to the specified size.
+# The interpolation method used is nearest-neighbor for the segmentation model.
+def load_file_nearest(size, file_path, depth=0):
+    file = cv2.imread(file_path, depth)
+    if depth == cv2.IMREAD_COLOR:
+        file = file[:, :, ::-1]
+    return cv2.resize(file, (size), interpolation=cv2.INTER_NEAREST)
+
+# load and resize the image to the specified size.
+# The interpolation method used is linear for the classification model.
+def load_file_linear(size, file_path, depth=0):
+    file = cv2.imread(file_path, depth)
+    if depth == cv2.IMREAD_COLOR:
+        file = file[:, :, ::-1]
+    return cv2.resize(file, (size), interpolation=cv2.INTER_LINEAR)
+
+
+@torch.inference_mode()
+def inference(model, class_model, image_paths, img_size, batch_size=24, device="cpu"):
+    num_images = len(image_paths)
+    num_batches = (num_images + batch_size - 1) // batch_size
+    
+    # Define mean and std
+    mean_seg = DatasetConfig.MEAN
+    std_seg = DatasetConfig.STD
+    mean_clf = DatasetConfig.MEAN_CLF
+    std_clf = DatasetConfig.STD_CLF
+    
+    overlay_paths = []
+    
+    time_acumm = 0
+
+    for batch_idx in range(num_batches):
+        start_idx = batch_idx * batch_size
+        end_idx = min((batch_idx + 1) * batch_size, num_images)
+        batch_diff = end_idx - start_idx + 1 
+        batch_images = []
+        batch_images_clf = []
+        # batch_image_org = []
+
+        for idx in range(start_idx, end_idx):
+            
+            # Load and preprocess image for classification
+            image_clf = load_file_linear(DatasetConfig.IMAGE_SIZE, image_paths[idx], depth=cv2.IMREAD_COLOR)
+            image_clf = normalize_classif(image_clf, mean_clf, std_clf)
+            
+            # Load and preprocess image and mask file, both for segmentation
+            image_org = load_file_nearest(DatasetConfig.IMAGE_SIZE, image_paths[idx], depth=cv2.IMREAD_COLOR)
+            image_norm = transforms.ToTensor()(image_org) #-- that is from older version
+        
+            # batch_image_org.append(image_org)
+            batch_images.append(image_norm)
+            batch_images_clf.append(image_clf)
+
+        start_model = time.perf_counter()
+        
+        # Classification predictions
+        # print(f"type: classifying")
+        batch_images_clf = np.stack(batch_images_clf)
+        y_pred_clf = class_model.predict(batch_images_clf, verbose=0).reshape(-1)
+        clf_labels = y_pred_clf > DatasetConfig.THR
+        true_idxs = np.where(clf_labels == True)[0]
+        
+        # Segmentation predictions
+        batch_images = torch.stack(batch_images).to(device)
+        mask_dim = (batch_diff, DatasetConfig.IMAGE_SIZE[0], DatasetConfig.IMAGE_SIZE[1])
+        pred_all = torch.Tensor(np.zeros(mask_dim)).long()
+        if len(true_idxs) > 0:
+            predictions = model(batch_images[true_idxs])
+            pred_all[true_idxs] = torch.from_numpy(predictions.argmax(dim=1).cpu().numpy())
+
+        # batch_images = torch.stack(batch_images).to(device)
+        # predictions = model(batch_images)
+        # pred_all = torch.from_numpy(predictions.argmax(dim=1).cpu().numpy())
+
+        end_model = time.perf_counter()
+        time_acumm = time_acumm + (end_model - start_model)
+        
+        # Apply overlay to each batch image
+        for i in range(len(batch_images)):
+            batch_img_np = batch_images[i].permute(1, 2, 0).numpy()  # Convert to numpy
+            pred_mask_rgb = num_to_rgb(pred_all[i], color_map=id2color)
+            overlay_img = image_overlay(batch_img_np, pred_mask_rgb)
+            
+            # Get the filename from the original image path
+            save_path = 'static/uploads/overlaid'
+            mask_save_path = 'static/uploads/masks'  # New path for saving masks
+            filename = os.path.splitext(os.path.basename(image_paths[start_idx + i]))[0]
+            overlay_filename = os.path.join(save_path, f"{filename}_overlaid.png")
+            mask_filename = os.path.join(mask_save_path, f"{filename}_mask.png")  # Mask filename
+            
+            # Save the overlaid image
+            cv2.imwrite(overlay_filename, (overlay_img * 255).astype(np.uint8), [cv2.IMWRITE_PNG_COMPRESSION, 9])
+            # Save the mask image
+            cv2.imwrite(mask_filename, (pred_mask_rgb * 255).astype(np.uint8), [cv2.IMWRITE_PNG_COMPRESSION, 9])
+            
+            overlay_paths.append(overlay_filename)
+            
+    print(f"modeling time: {time_acumm}")
+    
+    return overlay_paths
+
+def get_model(*, model_name, num_classes):
+    model = SegformerForSemanticSegmentation.from_pretrained(
+        model_name,
+        num_labels=num_classes,
+        ignore_mismatched_sizes=True,
+    )
+    
+    return model
+def get_class_model(class_model_loc, class_model):
+    # classification model instantiation
+    class_model_path = os.path.join(class_model_loc, class_model)
+    class_model = load_model(class_model_path)
+    return class_model
+    
+# loading checkpoint and model -- maybe make this a function so it can live here? 
+def predict(image_paths):
+    
+    CKPT_PATH = '/Users/dianawalsh/dats-data/static/ckpt_epoch50.ckpt'
+    model = MedicalSegmentationModel.load_from_checkpoint(CKPT_PATH)
+            
+    DEVICE = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    model.to(DEVICE)
+    model.eval()
+    
+    # predictions classification
+    class_model = 'resnet50v2_nn256_lr0001_relu_batch64_epoch30_v4.keras'
+    class_model_loc = 'static/'
+    class_model = get_class_model(class_model_loc, class_model)
+
+    predictions = inference(model, class_model, image_paths, img_size=DatasetConfig.IMAGE_SIZE, batch_size=10, device=DEVICE)
+    
+    return predictions
+
+
